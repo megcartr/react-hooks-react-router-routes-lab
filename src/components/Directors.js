2ef460f7bab0a2b44106ba874b12c322b2f3d88a
Meghan Carter <msc2010@yahoo.com>
2023-09-17 15:38:31 -0500

    lab completed

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,22 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  const directorNames = directors.map((director)=>(
+    <div key={director.name}>
+      {director.name}
+      <ul>
+        {director.movies.map((movie)=>(
+          <li key={movie}>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ))
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directorNames}
+      </div>
+  )
 }
 
 export default Directors;
